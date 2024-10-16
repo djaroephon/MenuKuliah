84ba00be394367257436c58cf6a438c23aa81271
Zettia <107917828+djaroephon@users.noreply.github.com>
2024-10-17 00:06:34 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import JadwalView from '@/views/JadwalView.vue'
+import JadwalView from '../views/JadwalView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
