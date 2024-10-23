fe325235d801565568cde723b100a635e5d322d3
Zettia <107917828+djaroephon@users.noreply.github.com>
2024-10-24 00:12:18 +0700

    Update SideBar.vue

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -3,13 +3,13 @@ import { ref } from 'vue';
 
 // Data for tasks
 const tugasKuliah = ref([
-{ title: 'Matematika Diskrit' },
-  { title: 'Algoritma Pemrograman' },
-  { title: 'Pengenalan Teknologi Informasi' },
-  { title: 'PENGENALAN PEMOGRAMAN' },
-  { title: 'PANCASILA' },
-  { title: 'AGAMA ISLAM' },
-  {title: 'KONSEP SISTEM INFORMASI'},
+  { title: 'Matematika Diskrit', link: '/matematika-diskrit' },
+  { title: 'Algoritma Pemrograman', link: '/algoritma-pemrograman' },
+  { title: 'Pengenalan Teknologi Informasi', link: '/pengenalan-ti' },
+  { title: 'PENGENALAN PEMOGRAMAN', link: '/pengenalan-pemrograman' },
+  { title: 'PANCASILA', link: '/pancasila' },
+  { title: 'AGAMA ISLAM', link: '/agama-islam' },
+  { title: 'KONSEP SISTEM INFORMASI', link: '/konsep-sistem-informasi' },
 ]);
 
 const isDropdownOpen = ref(false);
@@ -19,128 +19,93 @@ const toggleDropdown = () => {
 };
 </script>
 
-<template >
+<template>
 <nav class="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
   <div class="px-3 py-3 lg:px-5 lg:pl-3">
     <div class="flex items-center justify-between">
       <div class="flex items-center justify-start rtl:justify-end">
         <button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-            <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-               <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
-            </svg>
-         </button>
-        <a href="/" class="flex ms-2 md:me-24">
+          <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+             <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+          </svg>
+        </button>
+        <router-link to="/" class="flex ms-2 md:me-24">
           <img src="../assets/images/logo-uui.png" class="h-8 me-3" alt="FlowBite Logo" />
           <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Menu Kuliah</span>
-        </a>
+        </router-link>
       </div>
       <div class="flex items-center">
-          <div class="flex items-center ms-3">
-            <div>
-              <button type="button" class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" aria-expanded="false" data-dropdown-toggle="dropdown-user">
-                <img class="w-12 h-12 rounded-full" src="../assets/images/pp.jpg" alt="user photo">
-              </button>
-            </div>
-            <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600" id="dropdown-user">
-              <div class="px-4 py-3" role="none">
-                <p class="text-sm text-gray-900 dark:text-white" role="none">
-                  Djaroephon Djohan 
-                </p>
-                <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
-                  Djaroephon@gmail.com
-                </p>
-                <a class="text-sm font-medium text-emerald-400 truncate dark:text-gray-300" href="https://djaroephon.netlify.app/">
-                  My Profile
-                </a>
-              </div>
+        <div class="flex items-center ms-3">
+          <div>
+            <button type="button" class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" aria-expanded="false" data-dropdown-toggle="dropdown-user">
+              <img class="w-12 h-12 rounded-full" src="../assets/images/pp.jpg" alt="user photo">
+            </button>
+          </div>
+          <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600" id="dropdown-user">
+            <div class="px-4 py-3" role="none">
+              <p class="text-sm text-gray-900 dark:text-white" role="none">
+                Djaroephon Djohan
+              </p>
+              <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
+                Djaroephon@gmail.com
+              </p>
+              <a class="text-sm font-medium text-emerald-400 truncate dark:text-gray-300" href="https://djaroephon.netlify.app/">
+                My Profile
+              </a>
             </div>
           </div>
         </div>
+      </div>
     </div>
   </div>
 </nav>
 
 <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-gray border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
-    <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
-      <ul class="space-y-2 font-medium">
-        <li>
-          <a href="/" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
-              <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
-              <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
-            </svg>
-            <span class="ms-3">Home</span>
-          </a>
-        </li>
-        <li>
-    <!-- Button with Dropdown Toggle -->
-    <button
-      @click="toggleDropdown"
-      type="button"
-      class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-      aria-controls="dropdown-example"
-      :aria-expanded="isDropdownOpen">
-      <!-- Icon -->
-      <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 21">
-        <path d="M4 19V6.2C4 5.0799 4 4.51984 4.21799 4.09202C4.40973 3.71569 4.71569 3.40973 5.09202 3.21799C5.51984 3 6.0799 3 7.2 3H16.8C17.9201 3 18.4802 3 18.908 3.21799C19.2843 3.40973 19.5903 3.71569 19.782 4.09202C20 4.51984 20 5.0799 20 6.2V17H6C4.89543 17 4 17.8954 4 19ZM4 19C4 20.1046 4.89543 21 6 21H20M9 7H15M9 11H15M19 17V21" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-      </svg>
-    
-      <!-- Label -->
-      <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Mata Kuliah</span>
-      <!-- Badge Notification for Task Count -->
-      <!-- <span v-if="tugasKuliah.length > 0" class="bg-red-500 text-white text-xs font-semibold ml-2 py-0.5 px-2 rounded-full">
-        {{ tugasKuliah.length }}
-      </span> -->
-      <!-- Dropdown Arrow Icon -->
-     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
-      </svg>
-    </button>
-    
-    <!-- Dropdown Menu -->
-    <ul v-show="isDropdownOpen" id="dropdown-example" class="py-2 space-y-2">
-      <li v-for="(tugas, index) in tugasKuliah" :key="index">
-        <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
-          {{ tugas.title }}
-        </a>
+  <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
+    <ul class="space-y-2 font-medium">
+      <li>
+        <router-link to="/" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+          <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+            <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
+            <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
+          </svg>
+          <span class="ms-3">Home</span>
+        </router-link>
+      </li>
+      <li>
+        <button
+          @click="toggleDropdown"
+          type="button"
+          class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+          aria-controls="dropdown-example"
+          :aria-expanded="isDropdownOpen">
+          <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 21">
+            <path d="M4 19V6.2C4 5.0799 4 4.51984 4.21799 4.09202C4.40973 3.71569 4.71569 3.40973 5.09202 3.21799C5.51984 3 6.0799 3 7.2 3H16.8C17.9201 3 18.4802 3 18.908 3.21799C19.2843 3.40973 19.5903 3.71569 19.782 4.09202C20 4.51984 20 5.0799 20 6.2V17H6C4.89543 17 4 17.8954 4 19ZM4 19C4 20.1046 4.89543 21 6 21H20M9 7H15M9 11H15M19 17V21" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+          <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Mata Kuliah</span>
+          <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
+          </svg>
+        </button>
+        <ul v-show="isDropdownOpen" id="dropdown-example" class="py-2 space-y-2">
+          <li v-for="(tugas, index) in tugasKuliah" :key="index">
+            <router-link :to="tugas.link" class="flex items-center w-full p-2 ps-11 transition duration-75 rounded-lg group hover:bg-gray-100 dark:hover:bg-gray-700">
+              <span>{{ tugas.title }}</span>
+            </router-link>
+          </li>
+        </ul>
       </li>
     </ul>
-  </li>
-        <li>
-          <a href="/jadwal" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
-              <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" />
-            </svg>
-            <span class="flex-1 ms-3 whitespace-nowrap">Jadwal Kuliah</span>
-          </a>
-        </li>
-      </ul>
-    </div>
-  </aside>
+  </div>
+</aside>
 </template>
 
 <style scoped>
-/* Add styles for the sidebar and responsive behavior */
-body {
-  transition: margin-left 0.3s;
+nav {
+  background-color: #2c3e50;
 }
 
-#logo-sidebar {
-  transition: transform 0.3s ease;
-}
-
-.bg-gray {
-  background-color: #f9fafb; /* Change the color as needed */
-}
-
-.fixed {
-  transition: margin-left 0.3s;
-}
-
-/* Mobile adjustments */
-@media (max-width: 640px) {
-  .p-6 {
-    padding-left: 0; /* Reset padding for mobile */
-  }
+.router-link-active {
+  color: #42b983;
 }
 </style>
