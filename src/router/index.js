57f5272258a5745a3034a4122d6dd46ce7069a32
Zettia <107917828+djaroephon@users.noreply.github.com>
2024-10-24 14:07:01 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import JadwalView from '../views/JadwalView.vue'
+import TodoView from '@/views/TodoView.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,6 +19,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: JadwalView,
+    },
+    {
+      path: '/todo',
+      name: 'Todo',
+      component: TodoView
     }
   ]
 })
